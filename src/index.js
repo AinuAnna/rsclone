3bd323ef1e5af8544e652e39753d03bd30b8dfb3
Anna Tereshko <anna.26.tereshko@gmail.com>
2020-12-31 00:16:59 +0300

    feat: add index.html and styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './sass/style.scss';
+
+function ToggleMenuButton() {
+  const buttons = global.document.querySelectorAll('.toggle');
+
+  buttons.forEach((el) =>
+    el.addEventListener('click', function () {
+      this.parentElement.classList.toggle('change');
+    })
+  );
+}
+ToggleMenuButton();
