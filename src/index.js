031e783481b95c2eb76f702a2445734c846bf812
Demchuk Tatyana <69cobak@mail.ru>
2021-01-08 20:09:11 +0300

    feat: add profile

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import './sass/style.scss';
 import './utils/FirebaseAuth/FirebaseAuth';
 import Admin from './components/Admin/index';
 import './events';
+import StudentProfile from './components/StudentProfile/StudentProfile';
 
-const admin = new Admin();
-admin.render();
+// const admin = new Admin();
+// admin.render();
+
+const st = new StudentProfile();
+st.render(document.getElementById('table__users', 123));
