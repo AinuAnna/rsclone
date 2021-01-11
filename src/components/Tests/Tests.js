0c25bfa381167b7045335d56e2ce5f42707962f6
Anna Tereshko <anna.26.tereshko@gmail.com>
2021-01-11 18:43:43 +0300

    feat: tests work with db

--- src/components/Tests/Tests.js
+++ src/components/Tests/Tests.js
@@ -2,40 +2,11 @@ import './Tests.scss';
 import UI from '../UIclass/UIclass';
 import { FirebaseDB } from '../../utils/FirebaseDB/FirebaseDB';
 
-const firebase = new FirebaseDB();
-const testsArray = [
-  {
-    id: '1',
-    question: '1. Модель  - это...',
-    option: {
-      1: 'а) некий новый объект, который отражает существенные особенности изучаемого объекта, явления или процесса',
-      2: 'б) отражение наиболее существенных признаков, свойств и отношений явлений, объектов или процессов предметного мира',
-      3: 'в) виртуальная среда',
-      4: 'г) система, неотличимая от моделируемого объекта в отношении некоторых свойств, полагаемых существенными, которые полагаются несущественными',
-    },
-    answer: {
-      1: 'a)',
-      2: 'б',
-      3: 'г)',
-    },
-  },
-  {
-    id: '2',
-    question: '2. Моделирование  - это...',
-    option: {
-      1: 'а) некий новый объект, который отражает существенные особенности изучаемого объекта, явления или процесса',
-      2: 'б) отражение наиболее существенных признаков, свойств и отношений явлений, объектов или процессов предметного мира',
-      3: 'в) виртуальная среда',
-    },
-    answer: {
-      1: 'a)',
-      2: 'б',
-    },
-  },
-];
 export default class Tests extends UI {
-  constructor() {
+  constructor(rootNode) {
     super();
+    this.rootNode = rootNode;
+    this.firebaseDB = new FirebaseDB();
   }
 
   renderM() {
@@ -43,16 +14,17 @@ export default class Tests extends UI {
     UI.renderElement(wrapper, 'h2', 'Тесты', ['class', 'tests__title']);
     const container = UI.renderElement(wrapper, 'div', null, ['class', 'tests__container']);
     const containerList = UI.renderElement(container, 'div', null, ['class', 'tests__containerList']);
-    testsArray.forEach(({ question, id, option, answer }) => {
+    this.testsArray.forEach(({ id, question, option, answer }) => {
       const p = UI.renderElement(containerList, 'div', question, ['class', 'tests__label']);
       const divInputs = UI.renderElement(p, 'div', null, ['data-id', id], ['class', 'tests__divInputs']);
-      const LabelList = Object.values(option).forEach((val) => {
+      option.forEach((value) => {
         const divInputList = UI.renderElement(divInputs, 'div', null, ['class', 'tests__divInputList']);
         const label = UI.renderElement(divInputList, 'label', null, ['class', 'tests__label']);
         UI.renderElement(label, 'input', null, ['type', 'checkbox'], ['class', 'tests__input'], ['name', 'question']);
-        UI.renderElement(label, 'label', val, ['class', 'tests__label-list'], ['for', 'question']);
+        UI.renderElement(label, 'label', value, ['class', 'tests__label-list'], ['for', 'question']);
       });
     });
+
     UI.renderElement(
       this.rootNode,
       'button',
@@ -62,19 +34,14 @@ export default class Tests extends UI {
     );
   }
 
-  //   setData(data) {
-  //     this.testsArray = data.filter(() => true);
-  //     // this.collection = this.testsArray.forEach((el) => el.id);
-  //     // this.options = this.questions.filter(() => true);
-  //     // this.answers = this.questions.filter(() => true);
-  //   }
+  setData(tests) {
+    this.testsArray = tests.flat().map((el) => el);
+  }
 
-  render(rootNode) {
-    this.rootNode = rootNode;
-    this.renderM();
-    // firebase.getData('Tests').then((data) => {
-    //   // this.setData(data);
-
-    // });
+  render() {
+    this.firebaseDB.getTests().then((data) => {
+      this.setData(data);
+      this.renderM();
+    });
   }
 }
