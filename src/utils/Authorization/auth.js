8481143a96b54200dfe225192703f65391e2f183
Ivan <raggamix@mail.ru>
2021-01-13 00:09:01 +0300

    feat: add security auth

--- src/utils/Authorization/auth.js
+++ src/utils/Authorization/auth.js
@@ -8,20 +8,19 @@
 // const auth = firebase.auth();
 
 // add firebase data/guides
-/*
-db.collection('guides')
-  .get()
-  .then((snapshot) => {
-    setupGuides(snapshot.docs);
-});
-*/
 
 // listen for auth status changes
-auth.onAuthStateChanged(user => {
+auth.onAuthStateChanged((user) => {
   if (user) {
     console.log('user logged in: ', user);
+    db.collection('guides')
+      .get()
+      .then((snapshot) => {
+      setupGuides(snapshot.docs);
+      });
   } else {
     console.log('user logged out');
+    setupGuides([]);
   }
 });
 
