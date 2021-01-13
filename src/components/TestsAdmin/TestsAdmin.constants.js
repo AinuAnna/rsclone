703692a9ec427fb22989af975919e18d5752dc5d
d.ananyev <dimit999d@gmail.com>
2021-01-13 13:57:43 +0300

    feat: add art of add method

--- src/components/TestsAdmin/TestsAdmin.constants.js
+++ src/components/TestsAdmin/TestsAdmin.constants.js
@@ -1,3 +1,21 @@
 const deleteGroupTestsYesBtn = document.querySelector('#delete-test-yes');
 
-export default deleteGroupTestsYesBtn;
+function getTitle() {
+  const titleTestInput = document.querySelector('.title-test-input');
+  return titleTestInput;
+}
+
+function getQuestionInputs() {
+  const questionInputs = document.querySelectorAll('.question-input');
+  return questionInputs;
+}
+
+function getOptionInputs() {
+  const optionInputs = document.querySelectorAll('.option-input');
+  return optionInputs;
+}
+
+const addQuestBtn = document.querySelector('.add-question-btn');
+const addTestBtn = document.querySelector('.add-test-btn');
+
+export { deleteGroupTestsYesBtn, getTitle, getQuestionInputs, getOptionInputs, addQuestBtn, addTestBtn };
