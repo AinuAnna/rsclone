0a90e3f3956a5832c9b2c7b5100155284d6cdbe8
Anna Tereshko <anna.26.tereshko@gmail.com>
2021-01-16 23:24:24 +0300

    feat: add class Menu

--- src/components/Lecture/Lecture.js
+++ src/components/Lecture/Lecture.js
@@ -13,6 +13,31 @@ export default class TestResults extends UI {
     this.firebaseDB = new FirebaseDB();
   }
 
+  renderDefault() {
+    const wrapper = UI.renderElement(this.rootNode, 'div', null, ['class', 'lecture-admin__wrapper']);
+    UI.renderElement(wrapper, 'h2', 'Лекции', ['class', 'lecture-admin__title']);
+    const container = UI.renderElement(wrapper, 'div', null, ['class', 'lecture-admin__container']);
+    this.listMainTitle = UI.renderElement(container, 'div', 'Список лекций:', [
+      'class',
+      'lecture-admin__list-main-title',
+    ]);
+    const listTitle = UI.renderElement(this.listMainTitle, 'div', null, ['class', 'lecture-admin__list-title']);
+    const ol = UI.renderElement(listTitle, 'ol', null, ['class', 'lecture-admin__ul']);
+
+    /* Render lections --- PS MODIFY IT after implementation oof rendering */
+    this.lecturesArray.forEach(({ id, title, subtitle }) => {
+      UI.renderElement(listTitle, 'div', null, ['data-id', id], ['class', 'lecture-admin__div']);
+      const a = UI.renderElement(ol, 'a', null, ['class', 'lecture-admin__a'], ['href', '#']);
+      UI.renderElement(a, 'li', title, ['class', 'lecture-admin__li']);
+      subtitle.forEach((value) => {
+        const div1 = UI.renderElement(ol, 'div', null, ['data-id', id], ['class', 'lecture-admin__div']);
+        const ul1 = UI.renderElement(div1, 'ul', null, ['class', 'lecture-admin__ul']);
+        const a1 = UI.renderElement(ul1, 'a', null, ['class', 'lecture-admin__a'], ['href', '#']);
+        UI.renderElement(a1, 'li', value, ['class', 'lecture-admin__li']);
+      });
+    });
+  }
+
   renderM() {
     const wrapper = UI.renderElement(this.rootNode, 'div', null, ['class', 'lecture__wrapper']);
     const lArrow = UI.renderElement(
