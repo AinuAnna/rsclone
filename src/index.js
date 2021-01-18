e3cdeb05d9a64a8ecab80066a45552d08496a8a1
Demchuk Tatyana <69cobak@mail.ru>
2021-01-18 16:35:31 +0300

    feat: add nav to sidebar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import './sass/style.scss';
 // import Auth from './utils/Authorization/auth';
 import Event from './events';
+import Menu from './components/Menu/Menu';
 
 const event = new Event();
 event.renderFocus();
 // const auth = new Auth();
 // auth.AuthStateChanged();
+
+
+
