33e2a92ba340756629093064bbf80cad04bf2b27
Anna Tereshko <anna.26.tereshko@gmail.com>
2021-01-29 14:36:11 +0300

    feat: change port name

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const port = process.env.port || 4004;
+const port = process.env.PORT || 4004;
 const dirname = path.resolve();
 const app = express();
 const productRouter = express.Router();
